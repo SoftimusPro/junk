724502c34a5f9c69bbd069e6e6ac96409df029d4
SoftimusPro <darielanielys@gmail.com>
2023-03-24 11:30:29 -0400

    init commit

--- django_junk/settings.py
+++ django_junk/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventary'
+    'inventary',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- django_junk/urls.py
+++ django_junk/urls.py
@@ -14,15 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from inventary import views
+from django.urls import path, include
+from inventary import urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('signin/', views.signin, name='signin'),
-    path('signup/', views.signup, name='signup'),
-    path('inventary/', views.inventary, name='inventary'),
-    path('entry/', views.entry, name='entry'),
-    path('sell/', views.sell, name='sell'),
+    path('', include('inventary.urls')),
 ]
